dc44170cd973fce115d3698c7d2875e1ed20cc6c
Red-1-One <mesbah.redouan@gmail.com>
2023-03-18 02:22:46 +0100

    Hexadecimal update

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,9 +7,14 @@
  */
 int main(void)
 {
+	int n;
 	char ch;
 
-	for (ch = 'z'; ch >= 'a'; ch--)
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
 	{
 		putchar(ch);
 	}
