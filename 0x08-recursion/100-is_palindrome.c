0d25c76f364db5f31ff1d8694da286ae68582ceb
Red-1-One <mesbah.redouan@gmail.com>
2023-04-07 00:36:43 +0000

    palindrome update

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,7 +12,7 @@ int is_palindrome(char *s)
 {
 	if (*s == 0)
 		return (1);
-	return (check_pal (s, 0, _strlen_recursion (s)));
+	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
 /**
@@ -40,5 +40,5 @@ int check_pal(char *s, int i, int len)
 		return (0);
 	if (i >= len)
 		return (1);
-	return (check_pal (s, i + 1, len - 1));
+	return (check_pal(s, i + 1, len - 1));
 }
