305cce9448981d11dcc66bbd7cc7e868b13cf28d
Red-1-One <mesbah.redouan@gmail.com>
2023-04-12 03:28:13 +0000

    update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,10 +9,10 @@
 int count_word(char *s)
 {
 	int flag, c, w;
-	
+
 	flag = 0;
 	w = 0;
-	
+
 	for (c = 0; s[c] != '\0'; c++)
 	{
 		if (s[c] == ' ')
@@ -23,32 +23,31 @@ int count_word(char *s)
 			w++;
 		}
 	}
-	
-	return (w);		
+	return (w);
 }
 
 /**
  * **strtow - splits a string into words
- * @str: string to split		
+ * @str: string to split
  *
  * Return: pointer to an array of strings (Success)
- * or NULL (Error)		
+ * or NULL (Error)
  */
-char **strtow(char *str)		
+char **strtow(char *str)
 {
 	char **matrix, *tmp;
 	int i, k = 0, len = 0, words, c = 0, start, end;
-	
+
 	while (*(str + len))
 		len++;
 	words = count_word(str);
 	if (words == 0)
 		return (NULL);
-	
+
 	matrix = (char **) malloc(sizeof(char *) * (words + 1));
 	if (matrix == NULL)
 		return (NULL);
-	
+
 	for (i = 0; i <= len; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
